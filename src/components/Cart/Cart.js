62baa0681281f0e886fa1a6ce1145c8a6280bc41
wozszym <wozszym@gmail.com>
2020-09-04 16:52:18 +0100

    paypal button added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -20,7 +20,8 @@ export default class Cart extends Component {
                                     <CartColumns />
                                     <CartList value={value} />
 
-                                    <CartTotals value={value} />
+                                    <CartTotals value={value}
+                                        history={this.props.history} />
                                 </React.Fragment>
                             )            
                         }
